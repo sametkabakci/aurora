6583a3f97fea2d60c92c97c731f59b4817aed295
Samet Kabakçı <sametkabakci@Samet-Dizustu-Bilgisayar.local>
2024-07-12 20:22:16 +0300

    first commit

---
+++ app/(tabs)/bookmark.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Bookmark = () => {
+
+    return (
+        <View>
+            <Text>Bookmark</Text>
+        </View>
+    );
+};
+
+export default Bookmark;
